ed9aa007bcdef17ef3d03d6fd05c125e3139e5b4
raksha-rajpoot <rakraj654@gmail.com>
2025-04-19 00:50:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ function manipulateArray() {
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve([1, 2, 3, 4]);
+        }, 3000);
+      })
+      .then((array) => {
+        return new Promise((resolve) => {
+          const evens = array.filter(num => num % 2 === 0);
+          setTimeout(() => {
+            document.getElementById('output').textContent = evens.join(',');
+            resolve(evens);
+          }, 1000);
+        });
+      })
+      .then((evens) => {
+        return new Promise((resolve) => {
+          const doubled = evens.map(num => num * 2);
+          setTimeout(() => {
+            document.getElementById('output').textContent = doubled.join(',');
+            resolve(doubled);
+          }, 2000);
+        });
+      });
+    }
+
+    window.onload = manipulateArray;
+
